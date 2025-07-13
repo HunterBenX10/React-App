cb665ec8cb188cff314f44b415ca4146835a06ac
Ben <emmanuelbeneboh@gmail.com>
2025-07-13 19:50:19 -0400

    The original commit message is as follows: movie app react beginning

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const Card = ({ title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h2>Functional Component</h2>
+      <Card title="Star Wars" />
+      <Card title="Avatar" />
+      <Card title="Heretic" />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
